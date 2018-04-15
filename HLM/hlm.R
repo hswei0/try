688eeb4e033d47e392edcac764fe615bc64f02b1
hswei0 <github.wei@mailhero.io>
2018-04-16 01:10:06 +0800

    語法完成，要開始寫作業了

--- HLM/hlm.R
+++ HLM/hlm.R
@@ -1,26 +1,33 @@
 library(haven)
 library(tidyverse)
 library(magrittr)
-setwd("/Users/hsuwei/Desktop/try/HLM")
-l1 <- read_sav("newhwlevel1.sav")
-l2 <- read_sav("newhwlevel2.sav")
-
-
-mydata <- left_join(l1, l2, by = "schoolid") %>%
-  mutate(studentid = pupilNR_new, score = langPOST, IQ = IQ_verb)
-
 library(nlme)
 library(sjPlot)
 library(sjmisc)
-library(lme4)
 
-model1 <- lm(score ~ ses + IQ + sex, data = mydata)
+setwd("/Users/hsuwei/Desktop/try/HLM")
+mydata <- read_sav("newhwlevel1.sav")
+
+
+# 資料探勘 --------------------------------------------------------------------
+
+pacman::p_load(DataExplorer)
+
+plot_density(mydata)
+## `iq` 需要中心化、`ses` 已處理
+
+## 中心化
+mydata %<>%
+  mutate(iq.lm = scale(iq, scale = F)) #`scale` argument 會進行標準化
+
+# 分析 ----------------------------------------------------------------------
+
+model1 <- lm(score ~ ses + iq.lm + sex, data = mydata)
 summary(model1)
 
-model2 <- lme(fixed = score ~ ses + IQ + sex, random = ~1|schoolid, data = mydata)
-summary(model2)
 
-model2.1 <- lmer(score ~ ses + IQ + sex + (1|schoolid), data = mydata)
+
+model2 <- lmer(score ~ ses + IQ + sex + (1|schoolid), data = mydata)
 summary(model2.1)
 
 sjt.lm(model2.1, p.kr = FALSE)
@@ -40,3 +47,4 @@ model6.2 <- lmer(score ~ ses + IQ + sex + sch_ses_mean + sch_iqv_mean + (ses + I
 
 sjt.lm(model6.1, model6.2, p.kr = FALSE, show.icc = T, group.pred = TRUE)
 
+sjt.lm
